f38fc46af6a8f483c5a3c1ba408a121bd16c383a
Braian Hercun <94329348+HercunBraian@users.noreply.github.com>
2022-04-23 21:47:41 -0300

    Se modifica el index retirando el carrito debajo de los productos y dejando el carrito de la barra de navegacion

--- js/carrito copy.js
+++ js/carrito copy.js
@@ -1,7 +1,7 @@
 ﻿const cards = document.getElementById('cards')
 const items = document.getElementById('items')
 const footer = document.getElementById('footer');
-
+const procesarPedidoBtn = document.getElementById('procesar-pedido'); // Procesar Pedido
 const precioTotal = document.getElementById('precioTotal');
 const contadorTotal = document.getElementById('contadorTotal');
 const templateTotal = document.getElementById('template-total').content
@@ -11,8 +11,6 @@ const braian = document.getElementById('braian');
 const templateCompra = document.getElementById('template-compra').content
 const templateContador = document.getElementById('template-contador').content
 const templateCard = document.getElementById('template-card').content
-const templateCarrito = document.getElementById('template-carrito').content
-const templateFooter = document.getElementById('template-footer').content
 
 const fragment = document.createDocumentFragment();
 const fragment2 = document.createDocumentFragment();
@@ -33,7 +31,7 @@ document.addEventListener('DOMContentLoaded', e => {
   fetchData()
   if (localStorage.getItem('carrito')) {
       carrito = JSON.parse(localStorage.getItem('carrito'))
-      pintarCarrito()
+      mostrarCompra()
   }
 });
 
@@ -44,6 +42,18 @@ const fetchData = async () => {
   pintarCards(data)
 }
 
+//Eventos
+
+// Eliminamos el producto seleccionado
+braian.addEventListener('click', e =>{
+  eliminarProducto(e)
+})
+
+procesarPedidoBtn.addEventListener('click', e =>{
+  procesarPedido(e)
+})
+
+
 
 // Mostramos los productos en el Carrito
 const mostrarCompra = () =>{
@@ -53,6 +63,8 @@ const mostrarCompra = () =>{
     templateCompra.querySelector('h5').textContent = producto.nombre;
     templateCompra.querySelector('h6').textContent = producto.cantidad;
     templateCompra.querySelector('.cart-price').textContent = producto.cantidad * producto.precio;
+    templateCompra.querySelector('.delete-product').dataset.id = producto.id;
+
 
     const clone = templateCompra.cloneNode(true);
     fragment.appendChild(clone);
@@ -60,11 +72,8 @@ const mostrarCompra = () =>{
 
   braian.appendChild(fragment);
 
-  pintarFooter();
   mostrarTotal(); 
 
-  
-  
 }
 
 // Mostramos los productos en el HTML
@@ -115,62 +124,10 @@ const setCarrito = objeto =>{
    }
 
    carrito[producto.id] = {...producto}
-   pintarCarrito();
+   
    mostrarCompra();
   }
   
-// Mostramos los productos del carrito en el HTML
-
-  const pintarCarrito = () =>{
-   
-    items.innerHTML = '';
-    Object.values(carrito).forEach(producto =>{
-      templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre;
-      templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad;
-      templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio;
-
-      const clone = templateCarrito.cloneNode(true);
-      fragment.appendChild(clone);
-    })
-
-    items.appendChild(fragment);
-
-    pintarFooter();
-    mostrarCompra();
-
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-  }
-
-  const pintarFooter = () => {
-    footer.innerHTML = ''
-    
-    if (Object.keys(carrito).length === 0) {
-        footer.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
-        `
-        return
-    }
-    
-    // sumar cantidad y sumar totales
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
-    
-
-    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
-    templateFooter.querySelector('span').textContent = nPrecio
-
-    const clone = templateFooter.cloneNode(true)
-    fragment.appendChild(clone)
-
-    footer.appendChild(fragment)
-
-    const boton = document.querySelector('#vaciar-carrito')
-    boton.addEventListener('click', () => {
-        carrito = {}
-        pintarCarrito()
-        mostrarCompra();
-    })
-}
 
 const mostrarTotal = () => {
   precioTotal.innerHTML = ''
@@ -195,4 +152,33 @@ const mostrarTotal = () => {
 
   contadorTotal.appendChild(fragment3)
 
+}
+
+
+const eliminarProducto = e =>{
+  if (e.target.classList.contains('delete-product')) {
+    const producto = carrito[e.target.dataset.id]
+    if (producto.id == e.target.dataset.id) {
+      delete carrito[e.target.dataset.id]
+  } else {
+      carrito[e.target.dataset.id] = {...producto}
+  }
+  pintarCarrito()
+}
+}
+
+
+  const procesarPedido = e =>{
+    if (Object.keys(carrito).length === 0) {
+      Swal.fire({
+        type: 'error',
+        title: 'Oops...',
+        text: 'El carrito está vacío, agrega algún producto',
+        showConfirmButton: false,
+        timer: 2000
+    })
+}
+else {
+    location.href = "carrito.html";
+  }
 }
--- js/index.js
+++ js/index.js
@@ -11,8 +11,7 @@ const braian = document.getElementById('braian');
 const templateCompra = document.getElementById('template-compra').content
 const templateContador = document.getElementById('template-contador').content
 const templateCard = document.getElementById('template-card').content
-const templateCarrito = document.getElementById('template-carrito').content
-const templateFooter = document.getElementById('template-footer').content
+
 
 const fragment = document.createDocumentFragment();
 const fragment2 = document.createDocumentFragment();
@@ -33,7 +32,7 @@ document.addEventListener('DOMContentLoaded', e => {
   fetchData()
   if (localStorage.getItem('carrito')) {
       carrito = JSON.parse(localStorage.getItem('carrito'))
-      pintarCarrito()
+      mostrarCompra()
   }
 });
 
@@ -73,8 +72,7 @@ const mostrarCompra = () =>{
   })
 
   braian.appendChild(fragment);
-
-  pintarFooter();
+  localStorage.setItem('carrito', JSON.stringify(carrito))
   mostrarTotal(); 
 
 }
@@ -127,63 +125,9 @@ const setCarrito = objeto =>{
    }
 
    carrito[producto.id] = {...producto}
-   pintarCarrito();
    mostrarCompra();
   }
   
-// Mostramos los productos del carrito en el HTML
-
-  const pintarCarrito = () =>{
-   
-    items.innerHTML = '';
-    Object.values(carrito).forEach(producto =>{
-      
-      templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre;
-      templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad;
-      templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio;
-
-      const clone = templateCarrito.cloneNode(true);
-      fragment.appendChild(clone);
-    })
-
-    items.appendChild(fragment);
-
-    pintarFooter();
-    mostrarCompra();
-
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-  }
-
-  const pintarFooter = () => {
-    footer.innerHTML = ''
-    
-    if (Object.keys(carrito).length === 0) {
-        footer.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
-        `
-        return
-    }
-    
-    // sumar cantidad y sumar totales
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
-    
-
-    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
-    templateFooter.querySelector('span').textContent = nPrecio
-
-    const clone = templateFooter.cloneNode(true)
-    fragment.appendChild(clone)
-
-    footer.appendChild(fragment)
-
-    const boton = document.querySelector('#vaciar-carrito')
-    boton.addEventListener('click', () => {
-        carrito = {}
-        pintarCarrito()
-        mostrarCompra();
-    })
-}
 
 const mostrarTotal = () => {
   precioTotal.innerHTML = ''
@@ -219,7 +163,7 @@ const eliminarProducto = e =>{
   } else {
       carrito[e.target.dataset.id] = {...producto}
   }
-  pintarCarrito()
+  mostrarCompra()
 }
 }
 
